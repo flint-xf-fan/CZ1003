4405fee94438b4a612cdcd3b20d624d5455feabb
Xiaofeng <fxf1996@gmail.com>
2018-10-27 19:52:57 +0800

    added get_user_pos button

--- app.py
+++ app.py
@@ -1,68 +1,111 @@
 import pygame
+import sys
 
-# Define some colors
+
+FPS = 60
+SIZE = (1024, 720)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-pygame.init()
-
-# Set the width and height of the screen [width, height]
-size = (1024, 720)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("APP NAME To Be Decided")
-map_img = pygame.image.load("assets/NTUCampus.png")
-
-# Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+# necessary to display text to screen
+def text_objects(text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
 
 
 
+def get_user_pos(screen, time):
+    """
+    Makes the program halt for 'time' seconds or until the user performs the action.
+    """
+    clock = pygame.time.Clock()
+    waiting = True
+    print('Now click one point on the map to get your location')
+    while waiting:
+        dt = clock.tick(30) / 1000  # Takes the time between each loop and convert to seconds.
+        time -= dt
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                user_pos = event.pos
+                print('Your position is:', user_pos)
+                waiting = False
+                break
+ 
 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        if time <= 0:
+            waiting = False
+
+
+
+def main():
+    # settings
+    pygame.init()
+    
+    clock = pygame.time.Clock() # Used to manage how fast the screen updates
+
+
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("APP NAME To Be Decided")
+    
+    map_img = pygame.image.load("assets/NTUCampus.png")
+    map_x = 100
+    map_y = 100
+
+    button_x = 50
+    button_y = 50
+    button_w = 120
+    button_h = 50
+    button_getUserPos = pygame.Rect(button_x, button_y, button_w, button_h)  # creates a rect object
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            # # obtain mouse position on click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos  # gets mouse position
+
+                # checks if mouse position is over the button
+
+                if button_getUserPos.collidepoint(mouse_pos):
+                    # ask the user to click a position on the map
+                    get_user_pos(screen, 300)
+                    
+
+        screen.fill(WHITE)
+
+        ##### draing code goes here #####
+
+
+        ### get_user_pos button ###
+        mouse = pygame.mouse.get_pos()
+        if button_x < mouse[0] < button_x+button_w and button_y < mouse[1] < button_y+button_h: # mouse hover the button
+            pygame.draw.rect(screen, [255, 0, 0], button_getUserPos)
+            button_text = pygame.font.Font("freesansbold.ttf", int(button_h/2.5))
+        else:
+            pygame.draw.rect(screen, [200, 0, 0], button_getUserPos)  # button
+            button_text = pygame.font.Font("freesansbold.ttf", button_h//3)
+
+        textSurf, textRect = text_objects("get_user_pos", button_text)
+        textRect.center = (button_x+(button_w//2), button_y+(button_h//2))
+        screen.blit(textSurf, textRect)
         
-        # obtain mouse position on click
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print('mouse position test:', pos)
+        ### display the map ###
+        screen.blit(map_img, (map_x, map_y))
+        
+        
+        pygame.display.update()
+        clock.tick(FPS)
+        # print('Running test')
 
-    # --- Game logic should go here --
+    pygame.quit()
+    sys.exit
 
-
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    screen.fill(WHITE)
-    # screen.fill(pygame.image.load("NTUCampus.png"))
-    # screen.fill("NTUCampus.png")
-
-    # bg_img = pygame.image.load("NTUCampus.png")
-    bg_img_top = screen.get_height() - map_img.get_height()
-    bg_img_left = screen.get_width()/2 - map_img.get_width()/2
-    screen.blit(map_img,(bg_img_top,bg_img_left))
-
-
-    # --- Drawing code should go here
-
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-
-# Close the window and quit.
-pygame.quit()
+if __name__ == '__main__':
+    main()
